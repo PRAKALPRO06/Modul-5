5060f6842235424cfaff1fb278009d733d5fd374
deshanreal <oreokucinglucu99@gmail.com>
2024-11-03 22:15:26 +0700

    Create guided1.go

---
+++ 2311102326_Deshan Rafif Alfarisi/guided1.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+//Fungsi untuk mencetak bilanngan dari n hingga 1
+func cetakMundur(n int) {
+	if n == 1 {
+		fmt.Println(n)
+		return
+	}
+	fmt.Print(n, " ")
+	cetakMundur(n - 1)
+}
+
+func main() {
+	var n int
+	fmt.Print("Masukkan nilai n untuk cetak bilangan dari n hingga 1: ")
+	fmt.Scanln(&n)
+	fmt.Print("Hasil cetak mundur: ")
+	cetakMundur(n)
+}
