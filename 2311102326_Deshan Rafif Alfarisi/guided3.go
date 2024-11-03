47982a649c4763fe560045e9045671276b73f9c7
deshanreal <oreokucinglucu99@gmail.com>
2024-11-03 22:16:46 +0700

    Create guided3.go

---
+++ 2311102326_Deshan Rafif Alfarisi/guided3.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+//Fungsi untuk mencari 2 pangkat n
+func pangkatDua(n int) int {
+	if n == 0 {
+		return 1
+	}
+	return 2 * pangkatDua(n-1)
+}
+
+func main() {
+	var n int
+	fmt.Print("Masukkan nilai n untuk mencari 2 pangkat n: ")
+	fmt.Scanln(&n)
+	fmt.Println("Hasil 2 pangkat", n, ":", pangkatDua(n))
+}
