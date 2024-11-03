212b09ede6227d29c5340e4e419392a2ba706995
rezaalvonzo <161396429+rezaalvonzo@users.noreply.github.com>
2024-11-03 14:08:27 +0700

    modul 5

---
+++ 2311102026_Reza Alvonzo_Modul 5/unguided3.go
@@ -0,0 +1,29 @@
+package main
+import (
+	"fmt"
+)
+
+func findFactors(N, divisor int) {
+
+	if divisor > N {
+		return
+	}
+
+	if N%divisor == 0 {
+		fmt.Printf("%d ", divisor)
+	}
+
+	findFactors(N, divisor+1)
+}
+
+func main() {
+	var N int
+
+	fmt.Print("Masukkan bilangan bulat positif N: ")
+	fmt.Scan(&N)
+
+	fmt.Printf("Faktor-faktor dari %d adalah: ", N)
+	findFactors(N, 1) 
+	fmt.Println()
+}
+//Reza Alvonzo 2311102026
