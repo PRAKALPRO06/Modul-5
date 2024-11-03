d2dfec88ab6de70d46d083899b741dcccbab0660
fahri ramadhan <161453813+seiyaa11@users.noreply.github.com>
2024-11-03 21:35:30 +0700

    Add files via upload

---
+++ 2311102024_Fahri Ramadhan_MODUL5/unguided4.go
@@ -0,0 +1,25 @@
+package main
+import (
+	"fmt"
+)
+
+func printSeries(N, current int) {
+	if current == 1 {
+		fmt.Printf("%d ", current)
+		return
+	}
+
+	fmt.Printf("%d ", current)
+	printSeries(N, current-1)
+	fmt.Printf("%d ", current)
+}
+
+func main() {
+	var N int
+	fmt.Print("Masukkan bilangan bulat positif N: ")
+	fmt.Scan(&N)
+
+	fmt.Printf("Barisan bilangan dari %d hingga 1 dan kembali ke %d:\n", N, N)
+	printSeries(N, N)
+	fmt.Println()
+}
---
+++ 2311102024_Fahri Ramadhan_MODUL5/unguided5.go
@@ -0,0 +1,24 @@
+package main
+import (
+	"fmt"
+)
+
+func printOddNumbers(N, current int) {
+	if current > N {
+		return
+	}
+	if current%2 != 0 {
+		fmt.Printf("%d ", current)
+	}
+	printOddNumbers(N, current+1)
+}
+
+func main() {
+	var N int
+	fmt.Print("Masukkan bilangan bulat positif N: ")
+	fmt.Scan(&N)
+
+	fmt.Printf("Bilangan ganjil dari 1 hingga %d adalah:\n", N)
+	printOddNumbers(N, 1)
+	fmt.Println()
+}
